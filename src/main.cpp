569308e35e069b21f1e2875379a3a8bfa81bdbe1
Martin Haugaard <kullax@feral.dk>
2015-03-19 13:04:00 +0100

    Forbedret NFA print, passer nu overens med hackingoff.com/compilers/regular-expression-to-nfa-dfa

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ std::ostream& operator << (std::ostream &o, const Node &n){
 	std::list<Node*>::const_iterator i;
 	for( i = s.begin(); i != s.end(); ++i)
 	{
-		o << "node" << n.value  << " -> " << "node"<< (*i)->value << "[label=e]" << endl;
+		o << "node" << n.value  << " -> " << "node"<< (*i)->value << "[label=\"&epsilon;\"]" << endl;
 	}
 	return o;
 }
@@ -55,13 +55,13 @@ std::ostream& operator << (std::ostream &o, const World &w){
 	o << "  node [shape=circle]" << endl;
 
 // START NODE, FOR PRETTY PRINTING
-	o << "  node"<<w.start->value << " [shape=box]" << endl;
+	o << "  node"<<w.start->value << " [shape=circle]" << endl;
 	list<Node*> start = w.start->epsilons;
 	std::list<Node*>::const_iterator ii;
 
 	for( ii = start.begin(); ii != start.end(); ++ii)
 	{
-		o << "  node" << w.start->value  << " -> " << "node"<< (*ii)->value << "[label=e];" << endl;
+		o << "  node" << w.start->value  << " -> " << "node"<< (*ii)->value << "[label=\"&epsilon;\"];" << endl;
 	}
 
 	typedef map<char, Node*>::const_iterator MapIterator;
@@ -84,7 +84,7 @@ std::ostream& operator << (std::ostream &o, const World &w){
 			std::list<Node*>::const_iterator ii;
 			for( ii = s.begin(); ii != s.end(); ++ii)
 			{
-				o << "  node" << (*i)->value  << " -> " << "node"<< (*ii)->value << "[label=e];" << endl;
+				o << "  node" << (*i)->value  << " -> " << "node"<< (*ii)->value << "[label=\"&epsilon;\"];" << endl;
 			}
 
 			typedef map<char, Node*>::const_iterator MapIterator;
@@ -103,7 +103,7 @@ std::ostream& operator << (std::ostream &o, const World &w){
 
 	for( iii = end.begin(); iii != end.end(); ++iii)
 	{
-		o << "  node" << w.end->value  << " -> " << "node"<< (*iii)->value << "[label=e];" << endl;
+		o << "  node" << w.end->value  << " -> " << "node"<< (*iii)->value << "[label=\"&epsilon;\"];" << endl;
 	}
 
 	typedef map<char, Node*>::const_iterator MapIterator;
@@ -137,17 +137,15 @@ World* add_plus(World *world){
 }
 
 World* add_star(World *world){
-	Node *node1 = create_node(world);
-	Node *node2 = create_node(world);
-	Node *node3 = create_node(world);
-	node1->epsilons.push_back(world->start);
-	node1->epsilons.push_back(node3);
-	node2->epsilons.push_back(node1);
-	node2->epsilons.push_back(node3);
+	Node *start = create_node(world);
+	Node *end = create_node(world);
+	start->epsilons.push_back(world->start);
+	start->epsilons.push_back(end);
+	world->end->epsilons.push_back(world->start);
+	world->end->epsilons.push_back(end);
 
-	world->end->epsilons.push_back(node2);
-	world->start = node1;
-	world->end=node3;
+	world->start = start;
+	world->end=end;
 	return world;
 }
 
@@ -156,16 +154,12 @@ World* add_or(World *lworld, World *rworld){
 	world->count = lworld->count;
 	Node *start = create_node(world);
 	Node *end = create_node(world);
-	Node *right = create_node(world);
-	Node *left = create_node(world);
 	world->start=start;
 	world->end=end;
 	start->epsilons.push_back(lworld->start);
-	start->epsilons.push_back(right);
-	right->epsilons.push_back(rworld->start);
+	start->epsilons.push_back(rworld->start);
 	rworld->end->epsilons.push_back(end);
-	lworld->end->epsilons.push_back(left);
-	left->epsilons.push_back(end);
+	lworld->end->epsilons.push_back(end);
 	world->nodes.merge(lworld->nodes);
 	world->nodes.merge(rworld->nodes);
 	return world;
@@ -177,31 +171,87 @@ World* add_question(World *world){
 	return world;
 }
 
+World* connect(World *first, World *last){
+	World *world = new World();
+	world->count = first->count;
+	world->start = first->start;
+	world->end = last->end;
+//	first->end->epsilons.push_back(last->start);
+	first->end->epsilons.merge(last->start->epsilons);
+	last->nodes.remove(last->start);
+	world->nodes.merge(first->nodes);
+	world->nodes.merge(last->nodes);
+	return world;
+}
+
 int main(){
 	Counter *count = new Counter();
 	count->num=0;
+	// a
 	World *world1 = new World();
 	world1->count = count;
-	World *world2 = new World();
-	world2->count = count;
 	Node *node1 = create_node(world1);
 	Node *node2 = create_node(world1);
-
-	Node *node3 = create_node(world2);
-	Node *node4 = create_node(world2);
 	node1->nexts['a'] = node2;
 	world1->start=node1;
 	world1->end=node2;
+	// a*
+	world1 = add_star(world1);
 
+	// b
+	World *world2 = new World();
+	world2->count = count;
+	Node *node3 = create_node(world2);
+	Node *node4 = create_node(world2);
 	node3->nexts['b'] = node4;
 	world2->start=node3;
 	world2->end=node4;
 
-	World *w = add_or(world1, world2);
-//	cout << *w;
-//	World *ww = add_star(w);
-//	cout << *ww;
-	World *ww = add_question(w);
-	cout << *ww;
+	// cd
+	World *world3 = new World();
+	world3->count = count;
+	Node *node5 = create_node(world3);
+	Node *node6 = create_node(world3);
+	Node *node7 = create_node(world3);
+	node5->nexts['c'] = node6;
+	node6->nexts['d'] = node7;
+	world3->start=node5;
+	world3->end=node7;
+
+	// b|cd
+	World *bcd = add_or(world2, world3);
+
+	// (b|cd)*
+	bcd = add_star(bcd);
+
+	// a*(b|cd)*
+	World *full = connect(world1, bcd);
+	cout << *full;
+
+/*
+	// a
+	World *a = new World();
+	a->count = count;
+	Node *nodea = create_node(a);
+	Node *nodeaa = create_node(a);
+	nodea->nexts['a'] = nodeaa;
+	a->start=nodea;
+	a->end=nodeaa;
+	// a*
+	a = add_star(a);
+
+	// b
+	World *b = new World();
+	b->count = count;
+	Node *nodeb = create_node(b);
+	Node *nodebb = create_node(b);
+	nodeb->nexts['a'] = nodebb;
+	b->start=nodeb;
+	b->end=nodebb;
+	// b*
+	b = add_star(b);
+	World *full = connect(a, b);
+	cout << *full;
+*/
 	return 0;
 }
