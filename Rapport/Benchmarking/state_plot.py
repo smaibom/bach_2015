cfbc533096b9c6987d514823a86ff4e400698c0b
kullax <kullax@feral.dk>
2015-06-07 14:51:38 +0200

    TMaPa

--- Rapport/Benchmarking/state_plot.py
+++ Rapport/Benchmarking/state_plot.py
@@ -9,9 +9,11 @@ import matplotlib.pyplot as plt
 
 #x = np.linspace(0, 22, 22)
 
-grosses = [12550926,73555446,72870791,72870791,133865224,134413800,134413800,255427788,255427788,256276643,927802690]
-labels = ['0 0 0', '0 0 1', '0 1 0', '1 0 0',
+grosses = [62242364,123246883,122562228,122562228,183556657,184105105,184105105,243618278,245304681,245304681,305119181,305119181,305967853,977490276]
+labels = ['0 0 0',
+'0 0 1', '0 1 0', '1 0 0',
 '0 0 2', '0 2 0', '2 0 0 ',
+'0 0 3', '0 3 0', '3 0 0 ',
 '1 0 1', '0 1 1', '1 1 0',
 '1 1 1']
 
@@ -22,20 +24,21 @@ ax.ticklabel_format(style='plain')       # Remark 2
 ax.set_xticks(range(len(labels)))
 ax.set_xticklabels(labels, rotation=80)
 ax.bar(range(len(grosses)), grosses)
-plt.xlabel('Mismatches, deletions and insertions')
-plt.ylabel('Matching states created')
+plt.xlabel('alterations, deletions and insertions')
+plt.ylabel('states created')
 plt.show()
 
-#savefig('barchart.png', dpi=500)
-
-#0           =  12550926     0    hits
-#1ins        =  73555446     0    hits
-#1del        =  72870791     0    hits
-#1mut        =  72870791     0    hits
-#2ins        = 133865224     2    hits
-#2del        = 134413800     117  hits
-#2mut        = 134413800     117  hits
-#1ins 1mut   = 255427788     41   hits
-#1ins 1del   = 255427788     41   hits
-#1del 1mut   = 256276643     215  hits
-#1 1 1       = 927802690     3138 hits
+#0           =  62242364     0    hits
+#1ins        =  123246883     0    hits
+#1del        =  122562228     0    hits
+#1mut        =  122562228    0    hits
+#2ins        =  183556657     3    hits
+#2del        =  184105105     117  hits
+#2mut        =  184105105    117  hits
+#3ins        =  243618278     28   hits
+#3del        =  245304681     2066 hits
+#3mut        = 245304681     2066 hits
+#1ins 1mut   = 305119181     41   hits
+#1ins 1del   = 305119181     41   hits
+#1del 1mut   = 305967853     234  hits
+#1 1 1       = 977490276     3275 hits
