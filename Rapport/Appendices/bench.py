dc61f99478be28cf387abb95116e26c061663dc0
kullax <kullax@feral.dk>
2015-06-07 19:51:39 +0200

    Yir

--- Rapport/Appendices/bench.py
+++ Rapport/Appendices/bench.py
@@ -1,5 +1,5 @@
 import regex
 with open("../Data/chr22.txt") as myfile:
-    sequence = myfile.read().repalce('\n','').upper()
+    sequence = myfile.read().replace('\n','').upper()
 p = regex.compile("(?:TGCAAGCGTTAAT){s<=1,d<=0,i<=1}")
 print len(p.findall(sequence, overlapped=True))
